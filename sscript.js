2f27597eef73e1e43b601a6f904be248d3642008
Lorena <160654986+lorena511@users.noreply.github.com>
2024-06-21 23:20:35 -0400

    Add files via upload

---
+++ sscript.js
@@ -0,0 +1,20 @@
+// ejemplo de salida
+const superSensei = new Sensei("Master Splinter");
+showStats(nombre, fuerza, velocidad, salud, sabiduria);{
+    nombre= "Master Splimter";
+    fuerza= 10;
+    velocidad= 10;
+    salud= 10;
+    sabiduria= 10;
+}
+parentFunction(){
+    return "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.";
+}
+superSensei.speakWisdom();
+const mensaje= showStats.parentFunction;
+console.log(mensaje);
+// -> "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+superSensei.showStats();
+    salud += 200;
+console.log(nombre, salud, velocidad, fuerza);
+// -> "Nombre: Master Splinter, Salud: 210, Velocidad: 10, Fuerza: 10"
