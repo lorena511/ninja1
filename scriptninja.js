777a633828d92bbf73c4bd1e3b548040b492ac1a
Lorena <160654986+lorena511@users.noreply.github.com>
2024-06-21 22:46:53 -0400

    Add files via upload

---
+++ scriptninja.js
@@ -0,0 +1,20 @@
+const ninja = new ninja("Hyabusa");{  
+    showStats(nombre, fuerza, velocidad, salud);{
+  nombre= "Hyabusa";
+  fuerza= 3;
+  velocidad= 3;
+  salud= "salud";
+  } 
+ninja.nombre();{
+  nombre= newnombre;
+  newnombre= "Hyabusa";
+  }
+console.log(newnombre);
+
+ninja.showStats();{
+  salud += 10;
+  ninja.showStats= (nombre, fuerza, velocidad, salud);
+  message= ninja.showStats;
+  conloge.log(message);
+  }
+}
